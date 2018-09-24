091ab5b9ecc74af5128b2e7cc0617e08f5d51f4b
Victor Rodzko <vicrodzko@mail.ru>
2018-09-24 13:01:06 +0300

    Counter of consecutive numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  
+  if (array.length == 0) return 0;
+  if (array.length == 1) return 1;
+
+  array.sort((a, b) => a - b);
+  
+  let count = 1,
+      maxCount = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] - array[i-1] == 1) {
+        count++;
+      } else if (array[i] - array[i-1] > 1) {
+        maxCount = count > maxCount ? count : maxCount;
+        count = 1;
+      }
+  }
+  return maxCount;
 }
